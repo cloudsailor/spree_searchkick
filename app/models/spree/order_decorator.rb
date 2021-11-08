d4cb104d338e14d845a322479922ca0f7d46c383
Sky <befruitful12@gmail.com>
2021-11-08 14:22:55 -0500

    add multi search in order search box

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,65 @@
 module Spree::OrderDecorator
   def self.prepended(base)
     base.state_machine.after_transition to: :complete, do: :reindex_order_products
+    base.searchkick(
+      callbacks: :async,
+      word_start: [:name],
+      settings: { number_of_replicas: 0 },
+      index_prefix: ENV['SITE_NAME'],
+      merge_mappings: true,
+      mappings: {
+        properties: {
+          properties: {
+            type: 'nested'
+          }
+        }
+      }
+    ) unless base.respond_to?(:searchkick_index)
+
+    def base.autocomplete_fields
+      [:number]
+    end
+
+    def base.search_fields
+      [:number, :email, :skus, :firstname, :lastname, :state, :payment_state, :shipment_state, :promotions_ids, :store_id ,:channel]
+    end
+
+    def base.add_searchkick_option(option)
+      base.class_variable_set(:@@searchkick_options, base.searchkick_options.deep_merge(option))
+    end
+
+  end
+
+  def search_data
+    all_variants = variants.pluck(:id, :sku)
+    firstname = shipping_address&.firstname
+    lastname = shipping_address&.lastname
+    json = {
+      id: id,
+      number: number,
+      email: email,
+      skus: all_variants.map(&:last),
+      firstname: firstname,
+      lastname: lastname,
+      created_at: created_at,
+      state: state,
+      payment_state: payment_state,
+      shipment_state: shipment_state,
+      promotions_ids: promotions.pluck(:id),
+      store_id: store_id,
+      channel: channel,
+      completed_at: completed_at,
+      considered_risky: considered_risky,
+      approver_id: approver_id,
+      approved_at: approved_at,
+      group_buy: group_buy
+    }
+
+    json
+  end
+
+  def index_data
+    {}
   end
 
   def reindex_order_products
--- lib/spree/search/searchkick.rb
+++ lib/spree/search/searchkick.rb
@@ -1,6 +1,82 @@
 module Spree
   module Search
     class Searchkick < Spree::Core::Search::Base
+
+      require 'date'
+
+      def multi_keys_search(value, page, per_page)
+        multi_search_keys = ['number', 'email', 'firstname', 'lastname', 'skus']
+        orders = []
+        multi_search_keys.each do |key|
+          orders = order_base_elasticsearch(@order_args.merge({key => value}), page, per_page)
+          if orders.present?
+            break
+          end
+        end
+        orders
+      end
+
+      def retrieve_orders(args={}, page, per_page)
+        @order_args = {:approver_id => {_not: nil}}
+        params_before = {
+          :completed_at => "1",
+          :considered_risky => "1",
+          :group_buy => "1",
+          :approver_id => "1",
+        }
+        params_after = {
+          :completed_at => {_not: nil},
+          :considered_risky => true,
+          :group_buy => true,
+          :approver_id => {},
+        }
+
+        compare_keys = [:completed_at_lt, :completed_at_gt]
+
+        args.each do |key, value|
+          if (value&.present?) && (key != :number_cont) && (not compare_keys.include?(key))
+            @order_args = @order_args.merge({key => value})
+          end
+        end
+
+        compare_keys.each do |key|
+          if args[key].present?
+            if @order_args.key?(:completed_at) && @order_args[:completed_at] != "1"
+              @order_args[:completed_at] = @order_args[:completed_at].merge({key[-2,2].to_sym => args[key]})
+            else
+              @order_args[:completed_at] = {}
+              @order_args[:completed_at][key[-2,2].to_sym] = args[key]
+            end
+          end
+        end
+
+        params_before.each do |key, value|
+          if @order_args[key] == value
+            @order_args[key] = params_after[key]
+          end
+        end
+ 
+        if args[:number_cont].present?
+          @orders = multi_keys_search(args[:number_cont], page, per_page)
+        else
+          @orders =  order_base_elasticsearch(@order_args, page, per_page)
+        end
+
+      end
+
+      def order_base_elasticsearch(args={}, page, per_page)
+        curr_page = page || 1
+        Spree::Order.search(
+          keyword_query,
+          where: order_where_query(args),
+          aggs: aggregations,
+          smart_aggs: true,
+          order: {created_at: :desc},
+          page: curr_page,
+          per_page: per_page
+        )
+      end
+
       def retrieve_products(args={})
         @products = base_elasticsearch(args)
       end
@@ -27,6 +103,13 @@ module Spree
                   ]
               ]
         )
+      end
+
+      def order_where_query(args={})
+        where_query = {
+        }
+        where_query = where_query.merge(args)
+        add_search_filters(where_query)
       end
 
       def where_query(args={})
