0a2399f15e41c1786cf7511d3a4f7154ca10eafa
Neal Wong <neal.wkacc@gmail.com>
2022-11-03 16:54:22 -0500

    Removed order complete callbacks.

--- app/models/spree_searchkick/spree/order_decorator.rb
+++ app/models/spree_searchkick/spree/order_decorator.rb
@@ -1,19 +1,21 @@
 module SpreeSearchkick
   module Spree
     module OrderDecorator
-      def self.prepended(base)
-        base.state_machine.after_transition to: :complete, do: :reindex_order_products
-      end
+      # spree_searchkick only takes responsibility to reindex product after it save/updates, so no need to manually reindex here
 
-      def reindex_order_products
-        return unless complete?
+      # def self.prepended(base)
+      #   base.state_machine.after_transition to: :complete, do: :reindex_order_products
+      # end
 
-        if ::ActiveRecord::Base.connection.column_exists?(:spree_products, :conversions)
-          products.each {|product| product.update_column(:conversions, product.orders.complete.count) }
-        end
+      # def reindex_order_products
+      #   return unless complete?
 
-        products.map(&:reindex)
-      end
+      #   if ::ActiveRecord::Base.connection.column_exists?(:spree_products, :conversions)
+      #     products.each {|product| product.update_column(:conversions, product.orders.complete.count) }
+      #   end
+
+      #   products.map(&:reindex)
+      # end
     end
   end
 end
